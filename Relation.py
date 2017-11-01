9727779f361dc461cba4065374e77e107edf0137
Alan <alanoltman1@gmail.com>
2017-11-01 16:44:16 -0400

    yo

--- Relation.py
+++ Relation.py
@@ -1,5 +1,20 @@
 #All queries except for "E" go in here
 
-class Relation{
+class Relation:
+    def xMethod(name1, name2, relation):
+        if relation == 'parent':
+            if name1.getPare
+        if relation == 'sibling':
 
-}
+        if relation == 'half-sibling':
+
+        if relation == 'ancestor':
+
+        if relation == 'cousin':
+
+        if relation == 'unrelated':
+
+
+
+
+    
