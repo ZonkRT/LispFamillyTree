899165b0d70d8df92394db51809600cae54dc866
Jamie Chang <echang516@gmail.com>
2017-11-02 12:37:51 -0400

    I FIXED EVERYTHING MUAHAHAH

--- Main.py
+++ Main.py
@@ -1,85 +1,66 @@
+from person import person
+
 class run_tree:
     #queries = Relation() #for relation object
-    familyTree = list()
-    input = "E Bob Diana Matthew"
-    key, d1, d2, d3 = input.split()
-    if key == "E":
-        #check if familyTree has the person on them and if no, add to list
-        #p1 and p2 are the parents
-        p1 = {"Name": d1, "Value": Person(d1)}
-        p2 = {"Name": d2, "Value": Person(d2)}
+    familyTree = dict()
+    answer = "hi"
+    while (input != "no"):
+        answer = input()
+        key, d1, d2, d3 = answer.split()
+        if key == "E":
+            #add to family tree dictionary if not in familytree
+            if d1 not in familyTree:
+                familyTree[d1] = person(d1)
+            if d2 not in familyTree:
+                familyTree[d2] = person(d2)
+            if d3 != "":
+                if d3 not in familyTree:
+                    familyTree[d3] = person(d3)
+                    child = familyTree[d3]
+                    child.add_parents(familyTree[d1])
+                    child.add_parents(familyTree[d2])
 
-        print(p1["Value"].get_name())
-        print(p2["Value"].get_name())
+                    print("p3 parents: ")
+                    for a in familyTree[d3].get_parents():
+                        print(a.get_name())
+                    print()
 
-        """""
-        if p1 not in familyTree:
-            familyTree.append(p1)
-        else:
-            i = 0
-            while i < len(familyTree):
-                if familyTree[i]['Name'] == d1:
-                    p1 = familyTree[i]
-                    break
-                +i
+                    #added children to make a siblings list
+                    familyTree[d1].add_children(child)
+                    familyTree[d2].add_children(child)
 
-        if p2 not in familyTree:
-            familyTree.append(p2)
-        else:
-            i = 0
-            while i < len(familyTree):
-                if familyTree[i]['Name'] == d2:
-                    p2 = familyTree[i]
-                    break
-                +i
-        """
+                    print("p1 kids: ")
+                    for a in familyTree[d1].get_children():
+                        print(a.get_name())
+                    print()
+                    print("p2 kids: ")
+                    for a in familyTree[d2].get_children():
+                        print(a.get_name())
 
-        if d3 != "":
-            p3 = {"Name": d3, "Value": Person(d3)}
-            if p3 not in familyTree:
-                child = p3["Value"]
-                child.add_parents(p1["Value"])
-                child.add_parents(p2["Value"])
-
-                #added children to make a siblings list
-                p1["Value"].add_children(p3["Value"])
-                p2["Value"].add_children(p3["Value"])
-
-                c = Person("Elisa")
-                d = Person("Son")
-                e = Person("Nutter")
-                p1["Value"].add_children(c)
-                p1["Value"].add_children(d)
-
-                p2["Value"].add_children(c)
-                p2["Value"].add_children(e)
-
-                print("p1 kids: ")
-                for a in p1["Value"].get_children():
-                    print(a.get_name())
+                    #get children born from the two parents + make them kid's sibling
+                    temp1 = familyTree[d1].get_children()
+                    temp2 = familyTree[d2].get_children()
+                    temp = set(temp1).intersection(temp2)
+                    #check if siblings is not kid or already on list
+                    for a in temp:
+                        if a.get_name() != d3 and a not in child.get_siblings():
+                            child.add_siblings(a)
                 print()
-                print("p2 kids: ")
-                for a in p2["Value"].get_children():
+                print("p3 siblings: ")
+                for a in familyTree[d3].get_siblings():
                     print(a.get_name())
 
-                #get children born from the two parents + make them kid's sibling
-                temp1 = p1["Value"].get_children()
-                temp2 = p2["Value"].get_children()
-                temp = set(temp1).intersection(temp2)
-                #check if siblings is not kid or already on list
-                for a in temp:
-                    if a.get_name() != d3 and a not in child.get_siblings():
-                        child.add_siblings(a)
+        if key == 'W':
+            print("W")
+            #print(queries.wMethod(d1, d2))
+        if key == 'X':
+            print("X")
+            #print(queries.xMethod(d1, d3, d2))
 
-                familyTree.append(p3)
-            print()
-            print("p3 siblings: ")
-            for a in p3["Value"].get_siblings():
-                print(a.get_name())
 
-    if key == 'W':
-        print("W")
-        #print(queries.wMethod(d1, d2))
-    if key == 'X':
-        print("X")
-        #print(queries.xMethod(d1, d3, d2))
+
+
+
+
+
+
