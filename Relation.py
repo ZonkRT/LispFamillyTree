80461cb2586daab05c63ef6d1be932991fc1728f
Jamie Chang <echang516@gmail.com>
2017-11-02 00:02:36 -0400

    Added a thing for Andrew

--- Relation.py
+++ Relation.py
@@ -13,7 +13,18 @@ class Relation:
         if relation == 'cousin':
 
         if relation == 'unrelated':
-
+    def wMethod(name1, relation):
+        #a brainstorm to help you Andrew -Jamie
+        if relation == "half-siblings":
+            list1 = name1.get_parent()[0].get_children()
+            list2 = name1.get_parent()[1].get_children()
+            halfies = list()
+            temp = list(set (list1.append(list2)))
+            for a in temp:
+                for b in name1.get_sibling():
+                    if a != b:
+                        halfies.append(a)
+            return list(set(halfies))
 
 
 
