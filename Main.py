8f13699d23b6beca9ac190e91e6efe4c97f1c227
Jamie Chang <echang516@gmail.com>
2017-11-01 23:59:26 -0400

    Added minor changes, but still doesn't work >:(

--- Main.py
+++ Main.py
@@ -1,11 +1,16 @@
 class run_tree:
+    #queries = Relation() #for relation object
     familyTree = list()
     input = "E Bob Diana Matthew"
-    plist = input.split()
-    if plist[0] == "E":
+    key, d1, d2, d3 = input.split()
+    if key == "E":
         #check if familyTree has the person on them and if no, add to list
-        p1 = {"Name": plist[1], "Value": Person(plist[1])}
-        p2 = {"Name": plist[2], "Value": Person(plist[2])}
+        #p1 and p2 are the parents
+        p1 = {"Name": d1, "Value": Person(d1)}
+        p2 = {"Name": d2, "Value": Person(d2)}
+
+        print(p1["Value"].get_name())
+        print(p2["Value"].get_name())
 
         """""
         if p1 not in familyTree:
@@ -13,7 +18,7 @@ class run_tree:
         else:
             i = 0
             while i < len(familyTree):
-                if familyTree[i]['Name'] == plist[1]:
+                if familyTree[i]['Name'] == d1:
                     p1 = familyTree[i]
                     break
                 +i
@@ -23,20 +28,22 @@ class run_tree:
         else:
             i = 0
             while i < len(familyTree):
-                if familyTree[i]['Name'] == plist[2]:
+                if familyTree[i]['Name'] == d2:
                     p2 = familyTree[i]
                     break
                 +i
         """
 
-        if len(plist) == 4:
-            p3 = {"Name": plist[3], "Value": Person(plist[3])}
+        if d3 != "":
+            p3 = {"Name": d3, "Value": Person(d3)}
             if p3 not in familyTree:
                 child = p3["Value"]
-                child.add_parent(plist[1])
-                child.add_parent(plist[2])
-                p1["Value"].add_children(plist[3])
-                p2["Value"].add_children(plist[3])
+                child.add_parents(d1)
+                child.add_parents(d2)
+
+                #added children to make a siblings list
+                p1["Value"].add_children(d3)
+                p2["Value"].add_children(d3)
 
                 p1["Value"].add_children("Elisa")
                 p1["Value"].add_children("Son")
@@ -51,14 +58,17 @@ class run_tree:
                 temp1 = p1["Value"].get_children()
                 temp2 = p2["Value"].get_children()
                 temp = set(temp1).intersection(temp2)
+                #check if siblings is not kid or already on list
                 for a in temp:
-                    if a != plist[3] and a not in child.get_sibling():
-                        child.add_sibling(a)
+                    if a != d3 and a not in child.get_siblings():
+                        child.add_siblings(a)
 
                 familyTree.append(p3)
-            print("p3 siblings", p3["Value"].get_sibling())
+            print("p3 siblings", p3["Value"].get_siblings())
 
-    if plist[0] == 'W':
+    if key == 'W':
         print("W")
-    if plist[0] == 'X':
+        #print(queries.wMethod(d1, d2))
+    if key == 'X':
         print("X")
+        #print(queries.xMethod(d1, d3, d2))
